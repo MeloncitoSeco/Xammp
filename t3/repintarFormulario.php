9658323a1db86a4da75e22268907c28a14a06b64
MeloncitoSeco <109618733+MeloncitoSeco@users.noreply.github.com>
2023-10-17 10:47:14 +0200

    17/10/23

---
+++ t3/repintarFormulario.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Recibe parametros y repinta el formulario</h1>
+    <br>
+    <br>
+    <h2> campos de texto </h2>
+    <form>
+        <label for="fname">Nombre:</label>
+        <input type="text" id="nombre" name="nombre"><br>
+        <label for="lname">Contraseña:</label>
+        <input type="password" id="contra" name="contra">
+        <br>
+    </form>
+    <br><br>
+    <p>Choose your favorite Web language:</p>
+
+    <form>
+        <input type="radio" id="html" name="fav_language" value="HTML">
+        <label for="html">HTML</label><br>
+        <input type="radio" id="css" name="fav_language" value="CSS">
+        <label for="css">CSS</label><br>
+        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+        <label for="javascript">JavaScript</label>
+    </form>
+
+    <?php
+
+    ?>
+</body>
+</html>
